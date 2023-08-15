7a5feccfa6b017c8a6b5c5932be5bd5ad7443f61
Zaid9498 <136815199+Zaid9498@users.noreply.github.com>
2023-08-15 22:59:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -31,16 +31,20 @@ const lookup = {
   ",": ",",
 };
 
-function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
-  // Only change code below this line
-
-  return; //return decodedArr
+function rot13(str) {
+	let ans="";
+	for(let i=0;i<str.length;i++){
+		let currentChar=str[i].charCodeAt(0);
+		if(currentChar<=Z &&currentChar>=A){
+			ans +=lookup[str(i)]
+		}
+		else{
+			ans +=str[i];
+		}
+	}
+ 
+  return ans; //return decodedArr
 }
 
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
-
-// Do not change this line
 window.rot13 = rot13;
